3f08361849e192f8cdf7e73b8608b80b9b747084
pnaum <paul.naum@tremend.com>
2018-06-12 00:13:06 +0300

    ---

--- src/test/java/util/DriverBase.java
+++ src/test/java/util/DriverBase.java
@@ -13,7 +13,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-
 public class DriverBase extends BasePage {
 
     public DriverBase(WebDriver passedDriver) {
